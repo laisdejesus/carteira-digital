b5d0fcb723c420aad1e4ca2e755bddeaf5b55dd9
laisdejesus <dejesusll@outlook.com>
2025-05-12 23:04:07 -0300

    CDIG: update main

--- app/main.py
+++ app/main.py
@@ -1,24 +1,14 @@
 from fastapi import FastAPI
 
-from app.db import init_db
-from app.db.models import user
-from app.db.models import credit_user
 from app.routes.credit import credit_viewer
-from app.routes.users import users_viewer
 from app.routes.files import files_viewer
-from app.routes.users.controllers.user_controller import UserController
-from app.routes.files.controllers.files_controller import FilesController
-from app.routes.users.schemas.user_schema import UserSchema, UserCreate
+from app.routes.users import users_viewer
 
 
 app = FastAPI()
-#app = FastAPI(dependencies=[Depends(get_query_token)])
 
 
-#init_db()  #aparentemente isso aqui pode ser removido depois do almebic
-
-
-@app.get('/health')
+@app.get("/health")
 def health_check():
     return "ok, its health"
 
