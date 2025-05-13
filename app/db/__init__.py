cc2a0cbb0b33f770b995dd4f4408e28b7b68ba10
laisdejesus <dejesusll@outlook.com>
2025-05-13 02:42:30 -0300

    CDIG: config enviroment variables

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,10 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from app.settings import settings
+
 
 Base = declarative_base()
-DB_URL = "postgresql://fastapi_test:fastapi_test_password@localhost:5432/db"
-SQLALCHEMY_DATABASE_URL = (DB_URL)
+
+SQLALCHEMY_DATABASE_URL = (settings.DB_URL)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
