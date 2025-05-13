24b0dfab8ddb071bfcf796e1ca2fb39209e29048
laisdejesus <dejesusll@outlook.com>
2025-05-13 01:59:31 -0300

    CDIG: fix init db

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -4,10 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
-
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://fastapi_test:fastapi_test_password@localhost:5432/db"
-)
+DB_URL = "postgresql://fastapi_test:fastapi_test_password@localhost:5432/db"
+SQLALCHEMY_DATABASE_URL = (DB_URL)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
